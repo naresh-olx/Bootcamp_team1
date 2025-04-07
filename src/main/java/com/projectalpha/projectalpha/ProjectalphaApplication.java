3db0e467a7d772c601fc926440b23348a28a21e2
Nirbhay <nirbhay.gupta@olx.in>
2025-04-07 16:14:58 +0530

    project initilisation

---
+++ src/main/java/com/projectalpha/projectalpha/ProjectalphaApplication.java
@@ -0,0 +1,11 @@
+package com.projectalpha.projectalpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectalphaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectalphaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/projectalpha/projectalpha/ProjectalphaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectalpha.projectalpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectalphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
