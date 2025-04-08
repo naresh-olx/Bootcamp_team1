4223485bafbd5df8dcbd642e4a6f07946f6688bb
Nirbhay <nirbhay.gupta@olx.in>
2025-04-08 11:15:22 +0530

    health ckeck api added

--- src/main/java/com/projectalpha/projectalpha/controllers/inventoryApi.java
+++ src/main/java/com/projectalpha/projectalpha/controllers/inventoryApi.java
@@ -1,6 +1,15 @@
 package com.projectalpha.projectalpha.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("api")
 public class inventoryApi {
 
+    @GetMapping("/health-ok")
+    public String HealthOk(){
+        return "Health is OK !! 👌🏻😎";
+    }
 }
