555c9214af27c2c0c845ac3cbcd70f8d73d898a7
Nirbhay <nirbhay.gupta@olx.in>
2025-04-11 01:39:34 +0530

    user schema added again

--- src/main/java/com/projectalpha/projectalpha/entity/User.java
+++ src/main/java/com/projectalpha/projectalpha/entity/User.java
@@ -1,10 +1,13 @@
 package com.projectalpha.projectalpha.entity;
 
-import lombok.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.Date;
 import java.util.UUID;
 
 @Document(collection = "inventories")
@@ -12,12 +15,10 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class InventoryEntity {
-    @Id
-    private String sku;
-    private UUID carId;
-    private UUID createdBy;
-    private Date createdAt;
-    private UUID updatedBy;
-    private Date updatedAt;
+public class User {
+  @Id
+  private UUID userId;
+  private String userName;
+  private String password;
+  private String email;
 }
