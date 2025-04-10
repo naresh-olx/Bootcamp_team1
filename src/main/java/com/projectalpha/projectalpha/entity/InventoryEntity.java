e10125e215d51a3d633ba3622f1945c0e6f771fd
Nirbhay <nirbhay.gupta@olx.in>
2025-04-11 00:47:26 +0530

    filtering the entity

--- src/main/java/com/projectalpha/projectalpha/entity/InventoryEntity.java
+++ src/main/java/com/projectalpha/projectalpha/entity/InventoryEntity.java
@@ -14,53 +14,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class InventoryEntity {
     @Id
     private String sku;
-
     private String type;
-
     private String status;
-
     private String primaryLocation;
-
-//    private CarAttributes attributes;
-
-//    private Pricing pricing;
-
-//    private Metadata metadata;
-
-//    @Data
-//    @NoArgsConstructor
-//    @AllArgsConstructor
-//    @Builder
-//    public static class CarAttributes {
-//        private String vin;
-//        private String make;
-//        private String model;
-//        private String trim;
-//        private Integer year;
-//    }
-
-//    @Data
-//    @NoArgsConstructor
-//    @AllArgsConstructor
-//    @Builder
-//    public static class Pricing {
-//        private BigDecimal cost;
-//        private BigDecimal sellingPrice;
-//    }
-//
-//
-//    @Data
-//    @NoArgsConstructor
-//    @AllArgsConstructor
-//    @Builder
-//    public static class Metadata {
-//        @CreatedDate
-//        private Instant createdAt;
-//
-//        @LastModifiedDate
-//        private Instant updatedAt;
-//
-//        private String createdBy;
-//        private String updatedBy;
-//    }
 }
