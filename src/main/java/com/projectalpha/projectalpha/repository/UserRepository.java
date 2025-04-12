b1b57139aef3c058477d7a0cc779b9c8283950ff
Nirbhay <nirbhay.gupta@olx.in>
2025-04-12 17:49:59 +0530

    inventory service changed

--- src/main/java/com/projectalpha/projectalpha/repository/UserRepository.java
+++ src/main/java/com/projectalpha/projectalpha/repository/UserRepository.java
@@ -4,8 +4,6 @@ package com.projectalpha.projectalpha.repository;
 import com.projectalpha.projectalpha.entity.UserEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.UUID;
-
 public interface UserRepository extends MongoRepository<UserEntity, String> {
 
 }
