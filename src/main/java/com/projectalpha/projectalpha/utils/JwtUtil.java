dd22355c53b92c101a2c2582de0c0949f38bcfcc
Nirbhay <nirbhay.gupta@olx.in>
2025-04-13 11:47:28 +0530

    adding isTokenExpired() and generateToken() fxns

--- src/main/java/com/projectalpha/projectalpha/utils/JwtUtil.java
+++ src/main/java/com/projectalpha/projectalpha/utils/JwtUtil.java
@@ -4,11 +4,13 @@ import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
@@ -32,6 +34,19 @@ public class JwtUtil {
     public Date extractExpiration(String token) {
         Claims claims = extractAllClaims(token);
         return claims.getExpiration();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims,username);
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        return "";
     }
 
 
