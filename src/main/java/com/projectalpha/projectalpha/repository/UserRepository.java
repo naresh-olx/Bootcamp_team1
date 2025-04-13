ae1dfadd531ac43e32c2447d606d073020f6c907
Yash Sharma <yash.sharma@olx.in>
2025-04-13 13:07:20 +0530

    Add UserDetailsService in service layer

--- src/main/java/com/projectalpha/projectalpha/repository/UserRepository.java
+++ src/main/java/com/projectalpha/projectalpha/repository/UserRepository.java
@@ -5,5 +5,5 @@ import com.projectalpha.projectalpha.entity.UserEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<UserEntity, String> {
-
+    UserEntity findByEmailId(String emailId);
 }
