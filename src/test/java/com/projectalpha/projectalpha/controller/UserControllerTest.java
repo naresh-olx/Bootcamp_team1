a7292223a69c78bdf7cb5e50e768bd4bb545d1bb
Yash Sharma <yash.sharma@olx.in>
2025-04-13 21:33:24 +0530

    Add test handling success for signUp api

---
+++ src/test/java/com/projectalpha/projectalpha/controller/UserControllerTest.java
@@ -0,0 +1,58 @@
+package com.projectalpha.projectalpha.controller;
+
+import com.projectalpha.projectalpha.dto.UserRequestDTO;
+import com.projectalpha.projectalpha.dto.UserResponseDTO;
+import com.projectalpha.projectalpha.service.UserServices;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.http.HttpStatus.OK;
+
+class UserControllerTest {
+
+    @InjectMocks
+    private UserController userController;
+
+    @Mock
+    private UserServices userServices;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    private UserRequestDTO mockRequestDTO() {
+        UserRequestDTO userRequestDTO = new UserRequestDTO();
+        userRequestDTO.setUserName("test");
+        userRequestDTO.setPassword("test@123");
+        return userRequestDTO;
+    }
+
+    private UserResponseDTO mockResponseDTO() {
+        UserResponseDTO userResponseDTO = new UserResponseDTO();
+        userResponseDTO.setUserName("response-test");
+        userResponseDTO.setEmailId("response-test@email.com");
+        return userResponseDTO;
+    }
+
+    @Test
+    void createUser_Success() {
+        UserRequestDTO userRequestDTO = mockRequestDTO();
+        UserResponseDTO userResponseDTO = mockResponseDTO();
+
+        when(userServices.registerUser(userRequestDTO)).thenReturn(userResponseDTO);
+        ResponseEntity<?> response = userController.createUser(userRequestDTO);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(OK,response.getStatusCode());
+        assertEquals(userResponseDTO, response.getBody());
+        verify(userServices,times(1)).registerUser(userRequestDTO);
+    }
+}
