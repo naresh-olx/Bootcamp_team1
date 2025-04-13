b4267bbd4d3ffa2d1d659b257c3a2f058e297298
Nirbhay <nirbhay.gupta@olx.in>
2025-04-13 16:18:05 +0530

    creating user login dto

---
+++ src/main/java/com/projectalpha/projectalpha/dto/UserLoginDTO.java
@@ -0,0 +1,16 @@
+package com.projectalpha.projectalpha.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class UserLoginDTO {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String emailId;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 15, message = "Password must be between 6 and 15 characters long")
+    private String password;
+}
