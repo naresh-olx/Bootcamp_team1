5837a1f2c4527e330b6c0e7d6646c0879ec822c1
Nirbhay <nirbhay.gupta@olx.in>
2025-04-13 15:48:42 +0530

    removing saUser fxn

--- src/main/java/com/projectalpha/projectalpha/service/UserServices.java
+++ src/main/java/com/projectalpha/projectalpha/service/UserServices.java
@@ -26,34 +26,6 @@ public class UserServices {
     @Autowired
     private UserRepository userRepository;
 
-    public UserResponseDTO saveUser(UserRequestDTO userDTO) {
-        try{
-
-            boolean userExists = userRepository.existsById(userDTO.getEmailId());
-
-            if(!userExists){
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User doesn't exist with ID: " + userDTO.getEmailId());
-            }
-
-            UserEntity newUser = new UserEntity();
-            newUser.setUserName(userDTO.getUserName());
-            newUser.setEmailId(userDTO.getEmailId());
-            newUser.setPassword(passwordEncoder.encode(userDTO.getPassword()));
-            UserEntity createdUser =  userRepository.save(newUser);
-
-            UserResponseDTO saveUser = new UserResponseDTO();
-            saveUser.setEmailId(createdUser.getEmailId());
-            saveUser.setUserName(createdUser.getUserName());
-            saveUser.setUserId(createdUser.getUserId());
-
-            return saveUser;
-
-        } catch (Exception e){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User doesn't exist with ID: " + userDTO);
-        }
-
-    }
-
     public UserResponseDTO getUserById(String userId) {
         return null;
     }
