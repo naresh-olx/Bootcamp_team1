1a2c655ecb5781af035897fac2688beda36f2bdd
Naresh <naresh.bhadu@olx.in>
2025-04-17 18:08:21 +0530

    added naresh mongoDb string

--- src/main/java/com/projectalpha/projectalpha/ProjectalphaApplication.java
+++ src/main/java/com/projectalpha/projectalpha/ProjectalphaApplication.java
@@ -1,10 +1,8 @@
 package com.projectalpha.projectalpha;
 
-import com.projectalpha.projectalpha.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoAuditing
